dd5b38913efb3bb740cd3bbbc80e138d0b57923b
Kevin Gosse <gossekevin1@gmail.com>
2016-02-08 16:38:07 +0100

    use es7

--- js/app.js
+++ js/app.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import Hello from './components/Hello';
 import Main from './components/Main';
 
-ReactDOM.render(<Main />, document.getElementById('react'));
+ReactDOM.render(<Main limit={4}/>, document.getElementById('react'));
