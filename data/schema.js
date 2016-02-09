b984c8a467ab86ebbfb868613dfbb207086a9a5c
Kevin Gosse <gossekevin1@gmail.com>
2016-02-09 10:59:47 +0100

    fetch data using relay

--- data/schema.js
+++ data/schema.js
@@ -10,10 +10,19 @@ import {
     GraphQLString
 } from 'graphql';
 
-let counter = 42;
-let data = [42, 43, 44];
 
 let Schema = (db) => {
+    let store = {};
+
+    let storeType = new GraphQLObjectType({
+        name: 'Store',
+        fields: () => ({
+            links: {
+                type: new GraphQLList(linkType),
+                resolve: () => db.collection("links").find({}).toArray()
+            }
+        })
+    });
     let linkType = new GraphQLObjectType({
         name: 'Link',
         fields: () => ({
@@ -27,27 +36,9 @@ let Schema = (db) => {
         query: new GraphQLObjectType({
             name: 'Query',
             fields: () => ({
-                links: {
-                    type: new GraphQLList(linkType),
-                    resolve: () => db.collection('links').find({}).toArray()
-                },
-                counter: {
-                    type: GraphQLInt,
-                    resolve: () => 42
-                },
-                message: {
-                    type: GraphQLString,
-                    resolve: () => "Hello GraphQL"
-                }
-            })
-        }),
-
-        mutation: new GraphQLObjectType({
-            name: 'Mutation',
-            fields: () => ({
-                incrementCounter: {
-                    type: GraphQLInt,
-                    resolve: () => ++counter
+                store: {
+                    type: storeType,
+                    resolve: () => store
                 }
             })
         })
--- js/components/Main.js
+++ js/components/Main.js
@@ -2,12 +2,7 @@
  * Created by kevin gosse on 05/02/2016.
  */
 import React from 'react';
-import API from '../API';
-import LinkStore from '../stores/LinkStore';
-
-let _getAppState = () => {
-    return {links: LinkStore.getAll()};
-};
+import Relay from 'react-relay';
 
 class Main extends React.Component {
     static propTypes = {
@@ -18,23 +13,8 @@ class Main extends React.Component {
         limit: 3
     };
 
-    state = _getAppState();
-
-    onChange = () => {
-        console.log('4. In View');
-        this.setState(_getAppState);
-    };
-
-    componentDidMount(){
-        API.fetchLinks();
-        LinkStore.on('change', this.onChange)
-    }
-    componentWillUnmount(){
-        LinkStore.removeListener('change', this.onChange);
-    }
-
     render(){
-        let content = this.state.links.slice(0, this.props.limit).map(link => {
+        let content = this.props.store.links.slice(0, this.props.limit).map(link => {
             return (
                 <li key={link._id}>
                     <a href={link.url}>{link.title}</a>
@@ -51,5 +31,19 @@ class Main extends React.Component {
         )
     }
 }
+
+Main = Relay.createContainer(Main, {
+    fragments: {
+        store: () => Relay.QL`
+            fragment on Store {
+                links {
+                    _id,
+                    title,
+                    url,
+                }
+            }
+        `
+    }
+})
 
 export default Main;
